af7ea16ebe4c08f45df4413f26379dacc3dce63c
rahulsoni128 <rahulsoni.it22@scet.ac.in>
2025-01-07 04:15:28 +0000

    Intial

---
+++ 01_counter/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Card from './componts/Card'
+
+function App() {
+  return (
+    <>
+      <h1>Utsav Vachhani</h1>
+      <Card />
+    </>
+  );
+}
+
+export default App;
---
+++ 01_counter/src/componts/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Card(){
+    return(
+        <>
+            <h1>Rahul</h1>
+            <h3>Soni</h3>
+        </>
+    )
+}
+
+export default Card;
